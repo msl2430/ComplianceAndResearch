ad32cbaecb8d2826d27e3cf91b31df0c3569fd40
Mike <me@mikeslevine.com>
2015-05-02 11:29:06 -0400

    Test configuration

--- Opto 22/Opto22TestProject/Program.cs
+++ Opto 22/Opto22TestProject/Program.cs
@@ -16,14 +16,54 @@ namespace Opto22TestProject
             {
                 const int pointNumber = 12;
                 var optoMmp = new OptoMMP();
-                var connectionResult = optoMmp.Open(@"127.0.0.1", 22001, OptoMMP.Connection.TcpIp, 5000, false);
+                var connectionResult = optoMmp.Open(@"173.70.96.171", 2001, OptoMMP.Connection.TcpIp, 5000, false);
+                //var connectionResult = optoMmp.Open(@"127.0.0.1", 22001, OptoMMP.Connection.TcpIp, 5000, false);
                 //Console.WriteLine("Power up: " + optoMmp.WriteStatusCommand(OptoMMP.StatusWriteCommand.PowerUpClear));
                 
                 Console.WriteLine("Connection OptoMMP: " + connectionResult + " - " + optoMmp.IsCommunicationOpen);
-                string dateTime = "";
-                var result = optoMmp.ReadDateTime(out dateTime);
-                Console.WriteLine(result);
-                Console.WriteLine(dateTime);
+
+                if (optoMmp.IsCommunicationOpen)
+                {
+                    var result = optoMmp.WriteStatusCommand(OptoMMP.StatusWriteCommand.PowerUpClear);
+                    Console.WriteLine("PUC: " + result);
+                }
+
+                var device = new Station();
+                optoMmp.ReadBrainDiagnosticInformation(out device.structDiagInfo);
+
+                Console.WriteLine("Diag Info: " + device.structDiagInfo.sDevice);
+
+                var temp = new Int32[1];
+                for (var i = 0; i < 17; i++)
+                {
+                    var mmpAddress = 0xfffff0c00000 + i*0x100;
+                    var result = optoMmp.ReadInts(mmpAddress, 1, temp, 0);
+                    if(result < 0)
+                        Console.WriteLine("Error reading ints");
+                    else
+                        Console.WriteLine("Module " + i + " found: " + temp[0]);
+                }
+
+                Console.WriteLine("Current Operation: " + Enum.GetName(typeof(DeviceScanState), device.eState));
+
+                device.eState = DeviceScanState.Scanning;
+
+                Console.WriteLine("Switched Operation to " + Enum.GetName(typeof(DeviceScanState), device.eState));
+
+                Console.WriteLine("Current Operation: " + Enum.GetName(typeof(DeviceScanState), device.eState));
+                for(var i = 0; i < device.baryDiscreteStates64.Length; i++)
+                {
+                    //Console.WriteLine("Discreate State 64 " + i + ": " + device.baryDiscreteStates64[i]);
+                }
+                var test = optoMmp.ReadAnalogStates64(device.f32aryAnalogValues, 0);
+                Console.WriteLine("Analog States: " + test);
+                var singleResult = optoMmp.ReadSingles(0xfffff0500000, 6*4, device.f32aryAnalogValues, 0);
+                Console.WriteLine("Read single result: " + singleResult);
+                //string dateTime = "";
+
+                //var result = optoMmp.ReadDateTime(out dateTime);
+                //Console.WriteLine(result);
+                //Console.WriteLine(dateTime);
 
                 //if (optoMmp.IsCommunicationOpen)
                 //{
@@ -41,8 +81,7 @@ namespace Opto22TestProject
 
 
 
-                //var device = new Station();
-                //optoMmp.ReadBrainDiagnosticInformation(out device.structDiagInfo);
+                
                 
                 optoMmp.Close();
             }
